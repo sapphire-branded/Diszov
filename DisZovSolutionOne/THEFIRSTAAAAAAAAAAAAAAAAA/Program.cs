07b964a0d92009bff6507a844cb54a40ed189249
Disva <dravirdisva@gmail.com>
2016-11-03 23:00:16 -0400

    Added in base loop. Related to issue #1.

--- DisZovSolutionOne/THEFIRSTAAAAAAAAAAAAAAAAA/Program.cs
+++ DisZovSolutionOne/THEFIRSTAAAAAAAAAAAAAAAAA/Program.cs
@@ -10,6 +10,25 @@ namespace THEFIRSTAAAAAAAAAAAAAAAAA
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Disva and Zovael's Calculator!");
+            Console.WriteLine("Please make all deposits in rear~");
+            bool running = true;
+            String userInput = "";
+            while(running)
+            {
+                Console.WriteLine("Would you like to run a calculation?");
+                userInput = Console.ReadLine();
+                userInput = userInput.ToLower();
+                if (userInput.Equals("y") || userInput.Equals("yes"))
+                {
+                    Console.WriteLine("Well, too bad! That's not implemented!");
+                }
+                else
+                {
+                    running = false;
+                }
+            }
+            Console.Read();
         }
     }
 }
