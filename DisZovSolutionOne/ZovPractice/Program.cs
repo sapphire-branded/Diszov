2f0e78ad911f58b2db3a0835ceaa5f9e9ce51f2b
Zovael <Zovael@ZOVAEL-PC>
2017-01-13 19:49:02 -0800

    this is symbolic of everything wrong with how I work

--- DisZovSolutionOne/ZovPractice/Program.cs
+++ DisZovSolutionOne/ZovPractice/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 /*
  * Welcome to Disva's Code College, Zovael!
@@ -18,6 +20,51 @@ namespace ZovPractice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("P L E A S E");
+            Thread.Sleep(1000);
+            Console.WriteLine("I N S E R T");
+            Thread.Sleep(1000);
+            Console.WriteLine("T W O ( 2 )");
+            Thread.Sleep(1000);
+            Console.WriteLine("////");
+            Thread.Sleep(1000);
+            Console.Write("[inte");
+            Thread.Sleep(1000);
+            Console.WriteLine("ger]");
+            Thread.Sleep(1000);
+            Console.WriteLine("T O");
+            Thread.Sleep(1000);
+            Console.WriteLine("T H E");
+            Thread.Sleep(1000);
+            Console.WriteLine("G R I N D E R");
+            string NumberOneText = Console.ReadLine();
+            int NumberOne = int.Parse(NumberOneText);
+            Console.WriteLine("First number inserted. Thank you for your co-operation.");
+            string NumberTwoText = Console.ReadLine();
+            int NumberTwo = int.Parse(NumberTwoText);
+            Console.WriteLine("Second number inserted. Thank you for your co-operation.");
+            Thread.Sleep(1000);
+            Console.WriteLine("G R I N D I N G . . .");
+            Thread.Sleep(1000);
+            Console.WriteLine("G R I N D I N G . . .");
+            Thread.Sleep(1000);
+            Console.WriteLine("G R I N D I N G . . .");
+            int SumOfOnePlusTwo = (NumberOne + NumberTwo);
+            Thread.Sleep(2000);
+            Console.WriteLine("W E");
+            Thread.Sleep(1000);
+            Console.WriteLine("H A V E");
+            Thread.Sleep(1000);
+            Console.WriteLine("B E E N");
+            Thread.Sleep(1000);
+            Console.WriteLine("S A T E D");
+            Thread.Sleep(1000);
+            Console.WriteLine("////");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thank you for your assistance. Your result is: " + SumOfOnePlusTwo);
+            Thread.Sleep(5000);
+
+            Environment.Exit(0);
         }
     }
 }
