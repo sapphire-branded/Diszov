abcfeceae7d6a321c3c3eb914a346a8c51b7160a
Disva <dravirdisva@gmail.com>
2017-01-13 22:12:42 -0500

    Added comments and such

---
+++ DisZovSolutionOne/ZovPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Welcome to Disva's Code College, Zovael!
+ * Here, Disva will teach you all about coding!
+ * From Git to C# to design patterns, you'll learn it all!
+ * Using the handy dandy C# yellow book as reference, follow along!
+ */
+
+
+namespace ZovPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
